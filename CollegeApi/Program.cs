ea4c1774f0ba047c150765e88750d3fb0e715db4
Chahana Bhandari <chahanabhandari98@gmail.com>
2024-05-28 21:54:48 +0545

    data is pasted to college infrastructure and deleted

--- CollegeApi/Program.cs
+++ CollegeApi/Program.cs
@@ -8,5 +8,30 @@ app.MapGet("/courses/{id:int}", (int id, CollegeDbContext db) => db.Courses.Find
 app.MapGet("/sessions", (CollegeDbContext db) =>db.Sessions.ToList());
 app.MapGet("/sessions/{id:int}", (int id, CollegeDbContext db) => db.Sessions.Find(id));
 
+app.MapPost("/courses", (Course course, CollegeDbContext db) => {
+    db.Courses.Add(course);
+    db.SaveChanges();
+});
+
+app.MapPut("/courses", (Course course, CollegeDbContext db) => {
+    db.Courses.Update(course);
+    db.SaveChanges();
+});
+
+//app.MapDelete("/courses", (Course course, CollegeDbContext db) =>
+//{
+//    db.Courses.Remove(course);
+//    db.SaveChanges();
+//});
+
+//app.MapPost("/sessions", (Session session, CollegeDbContext db) => {
+//    db.Courses.Add(session);
+//    db.SaveChanges();
+//});
+
+//app.MapPut("/sessions", (Session session, CollegeDbContext db) => {
+//    db.Courses.Update(session);
+//    db.SaveChanges();
+//});
 
 app.Run();
