8561e1464f8fc0c1de41223fa812500a0de8fd0a
Chahana Bhandari <chahanabhandari98@gmail.com>
2024-05-30 10:22:33 +0545

    Course and session controller added and get,put,post,delete method is added

---
+++ CampusApi/Controllers/CourseController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CollegeAPI.Controllers
+{
+        [ApiController]
+        [Route("Courses")]
+        public class CourseController : ControllerBase
+        {
+            private readonly CollegeDbContext _context;
+            public CourseController(CollegeDbContext context)
+            {
+                _context = context;
+            }
+
+         [HttpGet]
+         public ActionResult<IEnumerable<Course>> Get()
+         {
+            return _context.Courses.ToList();
+         }
+         [HttpPost]
+         public async Task<ActionResult<Course>> Post(Course course)
+         {
+            _context.Courses.Add(course);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(Get), new { id = course.Id }, course);
+         }
+
+         [HttpPut("{id}")]
+         public async Task<IActionResult> Put(int id, Course course)
+         {
+            course.Id = id;
+            _context.Courses.Update(course);
+            await _context.SaveChangesAsync();
+            return NoContent();
+         }
+
+        
+         [HttpDelete("{id}")]
+         public async Task<IActionResult> Delete(int id)
+         {
+            var course = await _context.Courses.FindAsync(id);
+            if (course == null)
+            {
+                return NotFound("Course not found");
+            }
+            _context.Courses.Remove(course);
+            await _context.SaveChangesAsync();
+            return NoContent();
+
+         }
+
+        
+        }
+}
---
+++ CampusApi/Controllers/SessionController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CollegeAPI.Controllers
+{
+    [ApiController]
+    [Route("sessions")]
+    public class SessionController : ControllerBase
+    {
+        private readonly CollegeDbContext _context;
+        public SessionController(CollegeDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Session>> Get()
+        {
+            return _context.Sessions.ToList();
+        }
+        [HttpPost]
+        public async Task<ActionResult<Session>> Post(Session session)
+        {
+            _context.Sessions.Add(session);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(Get), new { id = session.Id }, session);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, Session session)
+        {
+            session.Id = id;
+            _context.Sessions.Update(session);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var session = await _context.Sessions.FindAsync(id);
+            if (session == null)
+            {
+                return NotFound("session not found");
+            }
+            _context.Sessions.Remove(session);
+            await _context.SaveChangesAsync();
+            return NoContent();
+
+        }
+
+
+        //[HttpPost]
+        //public string Post()
+        //{
+        //    return "Creating something on database.";
+        //}
+
+        //[HttpPut]
+        //public string Put()
+        //{
+        //    return "Updating something on database.";
+        //}
+
+        //[HttpDelete]
+        //public string Delete()
+        //{
+        //    return "Deleting something on database.";
+        //}
+    }
+}
